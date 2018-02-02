8c26d298c5b96828dcb4ce9e06ffdfedb35d3681
Salik Lennert Pedersen <saliklp@gmail.com>
2018-02-02 13:45:20 +0100

    read and save pcap to dataframe and h5 file

--- utils.py
+++ utils.py
@@ -2,22 +2,55 @@ from scapy.all import *
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import pandas as pd
 
 
-def savePcap(filename):
+def save_pcap(filename):
+    df = read_pcap(filename)
+    save_dataframe_h5(df, filename)
+
+
+def save_dataframe_h5(df, filename):
+    key = filename.split('.')[0]
+    df.to_hdf(filename + '.h5', key=key)
+
+
+def read_pcap(filename):
     timeS = time.clock()
+    df = pd.DataFrame(columns=['time', 'ip.dst', 'ip.src', 'protocol', 'port.dst', 'port.src', 'payload'])
+    count = 0
     data = rdpcap(filename)
+    timeR = time.clock()
+    timeRead = timeR-timeS
+    print("Time to read PCAP: "+ str(timeRead))
+    sessions = data.sessions()
+    for id, session in sessions.items():
+        for packet in session:
+            if IP in packet:
+                ip_layer = packet[IP]
+                frametime = packet.time
+                dst = ip_layer.dst
+                src = ip_layer.src
+                transport_layer = ip_layer.payload
+                protocol = transport_layer.name
+                dport = transport_layer.dport
+                sport = transport_layer.sport
+                payload = transport_layer.payload.original
+                df.loc[count] = [frametime, dst, src, protocol, dport, sport, payload]
+                count += 1
     timeE = time.clock()
     totalTime = timeE - timeS
-    print(totalTime)
-    npData = np.array(data)
-    savefile = filename + '.npy'
-    np.save(savefile, npData)
-
-# def convertPcap(data):
+    print("Time to convert PCAP to dataframe: " + str(totalTime))
+    return df
 
 
-
+def load_h5(filename, key=''):
+    timeS = time.clock()
+    df = pd.read_hdf(filename, key=key)
+    timeE = time.clock()
+    loadTime = timeE-timeS
+    print("Time to load " + filename + ": " + str(loadTime))
+    return df
 
 def plotHex(hexvalues, filename):
     '''
