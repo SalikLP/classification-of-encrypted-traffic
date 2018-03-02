988f7dd725a7a6ff7a9d31ce39407f5a99696d6c
Salik Lennert Pedersen <saliklp@gmail.com>
2018-03-02 09:47:31 +0100

    Stupid object dtype removed the hard way

--- pca/dataanalyzer.py
+++ pca/dataanalyzer.py
@@ -5,6 +5,19 @@ import pandas as pd
 import numpy as np
 
 
+def getmeanstd(dataframe, label):
+    labels = dataframe['label'] == label
+    values = dataframe[labels]['bytes'].values
+    bytes = np.zeros((values.shape[0], values[0].shape[0]))
+    for i, v in enumerate(values):
+        bytes[i] = v
+
+    # Ys = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+    mean = np.mean(bytes, axis=0)
+    mean_sub = np.subtract(bytes, mean)
+    std = mean_sub / np.std(bytes, axis=0)
+    return mean, mean_sub, std
+
 train_dir = '../../Data/h5/extracted/'
 dataframes = []
 for fullname in glob.iglob(train_dir + '*.h5'):
@@ -13,17 +26,8 @@ for fullname in glob.iglob(train_dir + '*.h5'):
     dataframes.append(df)
 # create one large dataframe
 data = pd.concat(dataframes)
-drtv = data['label'] == 'drtv'
-values = data[drtv]['bytes'].values
-values = np.reshape(values, (values.shape[0], 810))
-zero = np.zeros((values.shape[0], 810))
-new_values = np.add(values, zero)
-# Ys = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
-# values = values.astype(np.uint32)
-X_m = np.mean(new_values, axis=0)
-X_m= np.reshape(X_m, (1, X_m.shape[0]))
-X_ms= np.subtract(new_values, X_m)
-# X_ms = (new_values[0] - X_m)
-X_std = X_ms/np.std(new_values, axis=0)
-print(np.linalg.matrix_rank(X_std))
-print(values)
+dr_mean, dr_mean_sub, dr_std = getmeanstd(data, 'drtv')
+nf_mean, nf_mean_sub, nf_std = getmeanstd(data, 'netflix')
+print(dr_mean-nf_mean)
+
+
