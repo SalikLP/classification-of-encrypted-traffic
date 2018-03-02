1bfd0dc0d2ee87748d677a6abfd83b563d116af1
Salik Lennert Pedersen <saliklp@gmail.com>
2018-03-02 11:13:42 +0100

    Now with easier conversion of index to headerfiled and other stuff

--- pca/dataanalyzer.py
+++ pca/dataanalyzer.py
@@ -18,6 +18,65 @@ def getmeanstd(dataframe, label):
     std = mean_sub / np.std(bytes, axis=0)
     return mean, mean_sub, std
 
+
+def byteindextoheaderfield(number, TCP=True):
+    bytenumber = 0
+    if TCP:
+        bytenumber = number % 54
+    else:
+        bytenumber = number % 42
+    print(bytenumber)
+    if bytenumber in range(6):
+        return "Destination MAC"
+    if bytenumber in range(6, 12):
+        return "Source MAC"
+    if bytenumber in (12, 13):
+        return "Eth. Type"
+    if bytenumber == 14:
+        return "IP Version and header length"
+    if bytenumber == 15:
+        return "Explicit Congestion Notification"
+    if bytenumber in (16, 17):
+        return "Total Length (IP header)"
+    if bytenumber in (18, 19):
+        return "Identification (IP header)"
+    if bytenumber in (20, 21):
+        return "Fragment offset (IP header)"
+    if bytenumber == 22:
+        return "Time to live (IP header)"
+    if bytenumber == 23:
+        return "Protocol (IP header)"
+    if bytenumber in (24, 25):
+        return "Header checksum (IP header)"
+    if bytenumber in range(26, 30):
+        return "Source IP (IP header)"
+    if bytenumber in range(30, 34):
+        return "Destination IP (IP header)"
+    if bytenumber in (34, 35):
+        return "Source Port (TCP/UDP header)"
+    if bytenumber in (36, 37):
+        return "Destination Port (TCP/UDP header)"
+    if bytenumber in range(38, 42):
+        if TCP:
+            return "Sequence number (TCP header)"
+        elif bytenumber in (38, 39):
+            return "Length of data (UDP Header)"
+        else:
+            return "UDP Checksum (UDP Header)"
+    if bytenumber in range(42, 46):
+        return "ACK number (TCP header)"
+    if bytenumber == 46:
+        return "TCP Header length or Nonce (TCP header)"
+    if bytenumber == 47:
+        return "TCP FLAGS (CWR, ECN-ECHO, ACK, PUSH, RST, SYN, FIN) (TCP header)"
+    if bytenumber in (48, 49):
+        return "Window size (TCP header)"
+    if bytenumber in (50, 51):
+        return "Checksum (TCP header)"
+    if bytenumber in (52, 53):
+        return "Urgent Pointer (TCP header)"
+
+
 train_dir = '../../Data/h5/extracted/'
 dataframes = []
 for fullname in glob.iglob(train_dir + '*.h5'):
@@ -28,6 +87,15 @@ for fullname in glob.iglob(train_dir + '*.h5'):
 data = pd.concat(dataframes)
 dr_mean, dr_mean_sub, dr_std = getmeanstd(data, 'drtv')
 nf_mean, nf_mean_sub, nf_std = getmeanstd(data, 'netflix')
-print(dr_mean-nf_mean)
+mean_diff = dr_mean - nf_mean
+sort_diff = (-abs(mean_diff)).argsort() #Sort on absolute values in decending order
+for i in range(10):
+    print(byteindextoheaderfield(sort_diff[i]))
+# id_max = np.argmax(mean_diff)
+# print(byteindextoheaderfield(id_max))
+# id_min = np.argmin(mean_diff)
+# print(dr_mean-nf_mean)
+
+
 
 
